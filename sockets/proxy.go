Smilo-platform/go-connections

commit 61039d0834e53e8ae181e064229a9e79d7cd77aa

Replace deprecated Transport.Dial with Transport.DialContext
Signed-off-by: Paul Donohue <git@PaulSD.com>

sockets/sockets.go
@@ -32,7 +32,7 @@ func ConfigureTransport(tr *http.Transport, proto, addr string) error {
 		if err != nil {
 			return err
 		}
-		tr.Dial = dialer.Dial
+		tr.DialContext = dialer.DialContext
 	}
 	return nil
 }

sockets/sockets_unix.go
@@ -18,8 +18,11 @@ func configureUnixTransport(tr *http.Transport, proto, addr string) error {
 	}
 	// No need for compression in local communications.
 	tr.DisableCompression = true
-	tr.Dial = func(_, _ string) (net.Conn, error) {
-		return net.DialTimeout(proto, addr, defaultTimeout)
+	dialer := &net.Dialer{
+		Timeout: defaultTimeout,
+	}
+	tr.DialContext = func(ctx context.Context, _, _ string) (net.Conn, error) {
+		return dialer.DialContext(ctx, proto, addr)
 	}
 	return nil
 }

sockets/sockets_windows.go
@@ -15,7 +15,15 @@ func configureUnixTransport(tr *http.Transport, proto, addr string) error {
 func configureNpipeTransport(tr *http.Transport, proto, addr string) error {
 	// No need for compression in local communications.
 	tr.DisableCompression = true
-	tr.Dial = func(_, _ string) (net.Conn, error) {
+	dialer := &net.Dialer{
+		Timeout: defaultTimeout,
+	}
+	tr.DialContext = func(ctx context.Context, _, _ string) (net.Conn, error) {
+		// DialPipeContext() has been added to winio:
+		// https://github.com/Microsoft/go-winio/commit/5fdbdcc2ae1c7e1073157fa7cb34a15eab472e1d
+		// However, a new version of winio with this commit has not been released yet.
+		// Continue to use DialPipe() until DialPipeContext() becomes available.
+		//return winio.DialPipeContext(ctx, addr)
 		return DialPipe(addr, defaultTimeout)
 	}
 	return nil

commit c98bc5081b634c186f8a0947df4f211b2aaea702

Fix problems introduced by 61039d08
* Add missing import of "context" (Fixes
  https://github.com/docker/go-connections/issues/60)
* Drop configuration of the http.Transport.DialContext based on proxy
  settings (This was originally needed for SOCKS support, but has not
  been required since https://go-review.googlesource.com/c/go/+/35488/
  in Go 1.9, and is no longer supported as of
  https://go-review.googlesource.com/c/go/+/41031/ in Go 1.11)

sockets/proxy.go
@@ -5,8 +5,6 @@ import (
 	"net/url"
 	"os"
 	"strings"
-
-	"golang.org/x/net/proxy"
 )
 
 // GetProxyEnv allows access to the uppercase and the lowercase forms of
@@ -20,32 +18,8 @@ func GetProxyEnv(key string) string {
 	return proxyValue
 }
 
-// DialerFromEnvironment takes in a "direct" *net.Dialer and returns a
-// proxy.Dialer which will route the connections through the proxy using the
-// given dialer.
-func DialerFromEnvironment(direct *net.Dialer) (proxy.Dialer, error) {
-	allProxy := GetProxyEnv("all_proxy")
-	if len(allProxy) == 0 {
-		return direct, nil
-	}
-
-	proxyURL, err := url.Parse(allProxy)
-	if err != nil {
-		return direct, err
-	}
-
-	proxyFromURL, err := proxy.FromURL(proxyURL, direct)
-	if err != nil {
-		return direct, err
-	}
-
-	noProxy := GetProxyEnv("no_proxy")
-	if len(noProxy) == 0 {
-		return proxyFromURL, nil
-	}
-
-	perHost := proxy.NewPerHost(proxyFromURL, direct)
-	perHost.AddFromString(noProxy)
-
-	return perHost, nil
+// Deprecated: Proxies can now be configured using only http.Transport.Proxy
+// without configuring http.Transport.Dial.
+func DialerFromEnvironment(direct *net.Dialer) (*net.Dialer, error) {
+	return direct, nil
 }

sockets/sockets.go
@@ -3,7 +3,6 @@ package sockets
 
 import (
 	"errors"
-	"net"
 	"net/http"
 	"time"
 )
@@ -26,13 +25,6 @@ func ConfigureTransport(tr *http.Transport, proto, addr string) error {
 		return configureNpipeTransport(tr, proto, addr)
 	default:
 		tr.Proxy = http.ProxyFromEnvironment
-		dialer, err := DialerFromEnvironment(&net.Dialer{
-			Timeout: defaultTimeout,
-		})
-		if err != nil {
-			return err
-		}
-		tr.DialContext = dialer.DialContext
 	}
 	return nil
 }

sockets/sockets_unix.go
@@ -3,6 +3,7 @@
 package sockets
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"

sockets/sockets_windows.go
@@ -1,6 +1,7 @@
 package sockets
 
 import (
+	"context"
 	"net"
 	"net/http"
 	"time"